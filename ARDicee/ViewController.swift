27bdf8cd12943390c3b08d2318e68c7bdbe8d286
AndrewJenson <awjenson@gmail.com>
2018-08-17 16:16:11 -0700

    UI: AR of red dice added

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,36 +20,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
 
-        // Create geometry called cube
-        // Units are meters
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        // Create geometry called cube
+//        // Units are meters
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//
+//        let sphere = SCNSphere(radius: 0.2)
+//
+//        let materal = SCNMaterial()
+////        materal.diffuse.contents = UIColor.red
+//        materal.diffuse.contents = UIImage(named: "art.scnassets/8k_moon")
+//
+////        cube.materials = [materal]
+//        sphere.materials = [materal]
+//
+//        // Nodes are points in 3D space
+//        let node = SCNNode()
+//        // for z, negative is going away from you, positive coming towards you
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//
+//        // assign geometry
+////        node.geometry = cube
+//        node.geometry = sphere
+//
+//        sceneView.scene.rootNode.addChildNode(node)
 
-        let sphere = SCNSphere(radius: 0.2)
-
-        let materal = SCNMaterial()
-//        materal.diffuse.contents = UIColor.red
-        materal.diffuse.contents = UIImage(named: "art.scnassets/8k_moon")
-
-//        cube.materials = [materal]
-        sphere.materials = [materal]
-
-        // Nodes are points in 3D space
-        let node = SCNNode()
-        // for z, negative is going away from you, positive coming towards you
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-
-        // assign geometry
-//        node.geometry = cube
-        node.geometry = sphere
-
-        sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
 
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            // Set the scene to the view
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+
 
     }
     
